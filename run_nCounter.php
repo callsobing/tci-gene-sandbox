c89464b91c2f84b45167e28e91a76e4cd929387a
callsobing@gmail.com <lemontree>
2018-12-04 14:42:28 +0800

    test php run

--- run_nCounter.php
+++ run_nCounter.php
@@ -7,7 +7,7 @@
  */
 
 //$command = escapeshellcmd('python script/parse_nCounter_general.py');
-$command = exec('sudo -u www-data python script/test.py');
+$command = exec('sudo -u www-data python scripts/test.py');
 echo($command)
 
 ?>
