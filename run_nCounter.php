129df704ade22f62d4da8835eeef7e7e3c7f3a3b
callsobing@gmail.com <lemontree>
2018-12-04 19:52:37 +0800

    測試上傳資料

--- run_nCounter.php
+++ run_nCounter.php
@@ -17,6 +17,15 @@ $target_file = $target_dir.$_FILES["file_input"]["name"];
 echo($target_file);
 echo($_FILES['file_input']['tmp_name']);
 
+if ($_FILES["file"]["error"] > 0){
+    echo "Error: " . $_FILES["file"]["error"];
+}else{
+    echo "檔案名稱: " . $_FILES["file"]["name"]."<br/>";
+    echo "檔案類型: " . $_FILES["file"]["type"]."<br/>";
+    echo "檔案大小: " . ($_FILES["file"]["size"] / 1024)." Kb<br />";
+    echo "暫存名稱: " . $_FILES["file"]["tmp_name"];
+}
+
 if (move_uploaded_file($_FILES['file_input']['tmp_name'], $target_file)) {
     echo "File is valid, and was successfully uploaded.\\\\n";
 } else {
