42c3da8fa56bd3cde59cae5b1a1932a8994531e1
callsobing@gmail.com <lemontree>
2018-12-04 19:41:57 +0800

    測試上傳資料

--- chart.php
+++ chart.php
@@ -457,15 +457,16 @@
                                         </div>
                                     </div>
                                 </form>
+                                <div class="card-footer">
+                                    <button type="submit" class="btn btn-primary btn-sm">
+                                        <i class="fa fa-dot-circle-o"></i> Submit
+                                    </button>
+                                    <button type="reset" class="btn btn-danger btn-sm">
+                                        <i class="fa fa-ban"></i> Reset
+                                    </button>
+                                </div>
                             </div>
-                            <div class="card-footer">
-                                <button type="submit" class="btn btn-primary btn-sm">
-                                    <i class="fa fa-dot-circle-o"></i> Submit
-                                </button>
-                                <button type="reset" class="btn btn-danger btn-sm">
-                                    <i class="fa fa-ban"></i> Reset
-                                </button>
-                            </div>
+
                         </div>
                         <div class="row">
                             <div class="col-md-12">
