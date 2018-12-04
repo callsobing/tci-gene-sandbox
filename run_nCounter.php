8ef6e94c4bddcab607beb27705576c18c1cf99c8
callsobing@gmail.com <lemontree>
2018-12-04 14:08:11 +0800

    test php run

---
+++ run_nCounter.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yian.Tung
+ * Date: 2018/12/4
+ * Time: 下午 12:35
+ */
+
+//$command = escapeshellcmd('python script/parse_nCounter_general.py');
+$command = escapeshellcmd('python script/test.py');
