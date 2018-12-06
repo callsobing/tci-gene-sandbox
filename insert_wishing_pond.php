2308f727d01b0872601b16fa078724e0f7b7ff82
callsobing@gmail.com <lemontree>
2018-12-06 23:36:11 +0800

    加入上傳檔案的環節

--- insert_wishing_pond.php
+++ insert_wishing_pond.php
@@ -8,9 +8,9 @@
 
 // Start 如果有上傳檔案放到正確的地方
 $target_dir = "uploaded_files/wishing_pond/";
-$target_file = $target_dir.$_FILES["file_input"]["name"];
+$target_file = $target_dir.$_FILES["file_path"]["name"];
 
-if (move_uploaded_file($_FILES['file_input']['tmp_name'], $target_file)) {
+if (move_uploaded_file($_FILES['file_path']['tmp_name'], $target_file)) {
 } else {
     echo "Possible file upload attack!\\\\n";
 }
