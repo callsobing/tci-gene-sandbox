3a15078a1ed5385aaa1f97662131a17fe658fa34
callsobing@gmail.com <lemontree>
2018-12-12 14:13:21 +0800

    javascript post submit

--- get_nCounter_samples.php
+++ get_nCounter_samples.php
@@ -6,7 +6,7 @@
  * Time: 下午 12:35
  */
 
-$file_name = $_GET["file_select"];
+$file_name = $_POST["file_select"];
 echo($file_name);
 $command_inline = 'sudo -u www-data python3.4 scripts/nCounter_selection.py ' .  $file_name . ' ' . uniqid();
 $command = exec($command_inline);
