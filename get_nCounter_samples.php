a214c55dbbda13abb67240a925e602f0f1996953
callsobing@gmail.com <lemontree>
2018-12-12 15:56:37 +0800

    把按鍵包在form裡面

--- get_nCounter_samples.php
+++ get_nCounter_samples.php
@@ -361,10 +361,10 @@ $command = shell_exec("sudo -u www-data python3.4 scripts/nCounter_selection.py 
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="col col-md-9">
-                                    <button type="submit" class="btn btn-primary btn-sm">Submit</button>
-                                    <button type="reset" class="btn btn-danger btn-sm">Reset</button>
+                                    <div class="col col-md-9">
+                                        <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+                                        <button type="reset" class="btn btn-danger btn-sm">Reset</button>
+                                    </div>
                                 </div>
                             </form>
                         </div>
