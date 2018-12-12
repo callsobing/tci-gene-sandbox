60f85a0123dc62456178cb86445f4f6885a5ebd0
callsobing@gmail.com <lemontree>
2018-12-12 18:06:40 +0800

    加上專案名稱

--- get_nCounter_samples.php
+++ get_nCounter_samples.php
@@ -83,96 +83,34 @@ $command = shell_exec("sudo -u www-data python3.4 scripts/nCounter_selection.py 
             <div class="container-fluid">
                 <ul class="navbar-mobile__list list-unstyled">
                     <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-tachometer-alt"></i>Dashboard</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="index.php">Dashboard 1</a>
-                            </li>
-                            <li>
-                                <a href="index2.html">Dashboard 2</a>
-                            </li>
-                            <li>
-                                <a href="index3.html">Dashboard 3</a>
-                            </li>
-                            <li>
-                                <a href="index4.html">Dashboard 4</a>
-                            </li>
-                        </ul>
+                        <a class="js-arrow" href="index.php">
+                            <i class="fas fa-tachometer-alt"></i>資訊主頁
+                        </a>
                     </li>
                     <li>
                         <a href="new_project.php">
-                            <i class="fas fa-chart-bar"></i>Charts</a>
+                            <i class="fas fa-chart-bar"></i>圖表專區
+                        </a>
                     </li>
                     <li>
-                        <a href="table.html">
-                            <i class="fas fa-table"></i>Tables</a>
+                        <a href="wishing_pond.php">
+                            <i class="fa fa-magic"></i>許願池</a>
                     </li>
                     <li>
-                        <a href="form.html">
-                            <i class="far fa-check-square"></i>Forms</a>
+                        <a href="file_management.php">
+                            <i class="fas fa-table"></i>檔案管理</a>
                     </li>
                     <li>
                         <a href="#">
-                            <i class="fas fa-calendar-alt"></i>Calendar</a>
+                            <i class="far fa-check-square"></i>表格填寫</a>
                     </li>
                     <li>
-                        <a href="map.html">
-                            <i class="fas fa-map-marker-alt"></i>Maps</a>
+                        <a href="#">
+                            <i class="fas fa-calendar-alt"></i>儀器排程</a>
                     </li>
-                    <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-copy"></i>Pages</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="login.php">Login</a>
-                            </li>
-                            <li>
-                                <a href="register.html">Register</a>
-                            </li>
-                            <li>
-                                <a href="forget-pass.html">Forget Password</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-desktop"></i>UI Elements</a>
-                        <ul class="navbar-mobile-sub__list list-unstyled js-sub-list">
-                            <li>
-                                <a href="button.html">Button</a>
-                            </li>
-                            <li>
-                                <a href="badge.html">Badges</a>
-                            </li>
-                            <li>
-                                <a href="tab.html">Tabs</a>
-                            </li>
-                            <li>
-                                <a href="card.html">Cards</a>
-                            </li>
-                            <li>
-                                <a href="alert.html">Alerts</a>
-                            </li>
-                            <li>
-                                <a href="progress-bar.html">Progress Bars</a>
-                            </li>
-                            <li>
-                                <a href="modal.html">Modals</a>
-                            </li>
-                            <li>
-                                <a href="switch.html">Switchs</a>
-                            </li>
-                            <li>
-                                <a href="grid.html">Grids</a>
-                            </li>
-                            <li>
-                                <a href="fontawesome.html">Fontawesome Icon</a>
-                            </li>
-                            <li>
-                                <a href="typo.html">Typography</a>
-                            </li>
-                        </ul>
+                    <li>
+                        <a href="#">
+                            <i class="fas fa-map-marker-alt"></i>已出貨區域</a>
                     </li>
                 </ul>
             </div>
@@ -190,16 +128,22 @@ $command = shell_exec("sudo -u www-data python3.4 scripts/nCounter_selection.py 
         <div class="menu-sidebar__content js-scrollbar1">
             <nav class="navbar-sidebar">
                 <ul class="list-unstyled navbar__list">
-                    <li class="active has-sub">
-                        <a class="js-arrow" href="#">
-                            <i class="fas fa-tachometer-alt"></i>資訊主頁</a>
+                    <li class="has-sub">
+                        <a class="js-arrow" href="index.php">
+                            <i class="fas fa-tachometer-alt"></i>資訊主頁
+                        </a>
                     </li>
                     <li>
                         <a href="new_project.php">
-                            <i class="fas fa-chart-bar"></i>圖表專區</a>
+                            <i class="fas fa-chart-bar"></i>圖表專區
+                        </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="wishing_pond.php">
+                            <i class="fa fa-magic"></i>許願池</a>
+                    </li>
+                    <li>
+                        <a href="file_management.php">
                             <i class="fas fa-table"></i>檔案管理</a>
                     </li>
                     <li>
@@ -297,6 +241,15 @@ $command = shell_exec("sudo -u www-data python3.4 scripts/nCounter_selection.py 
                                     <h4>nCounter 報告設定</h4>
                                 </div>
                                 <div class="card-body">
+                                    <div class="row form-group">
+                                        <div class="col col-md-3">
+                                            <label for="text-input" class=" form-control-label">專案名稱</label>
+                                        </div>
+                                        <div class="col-12 col-md-9">
+                                            <input type="text" id="project_name" name="project_name" placeholder="請輸入你的專案名稱" class="form-control">
+                                            <small class="form-text text-muted">請使用與專案直接相關的敘述，方便未來辨識</small>
+                                        </div>
+                                    </div>
                                     <div class="custom-tab">
                                         <nav>
                                             <div class="nav nav-tabs" id="nav-tab" role="tablist">
