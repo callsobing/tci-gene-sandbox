3d172855246006d891bd2cdad63cf71dd2e9bf74
callsobing@gmail.com <lemontree>
2018-12-16 12:34:28 +0800

    純ｂａｒ圖

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -47,7 +47,7 @@ def plot_gene(gene_details_map, gene, user_id, report_uuid):
     mock_mean = average(gene_details_map[gene]["mock"]["fold_change"])
     mean_6h1 = average(gene_details_map[gene]["cond1"]["fold_change"])
     mean_6h2 = average(gene_details_map[gene]["cond2"]["fold_change"])
-    x_pos = (1, 2, 3)
+    x_pos = (0, 1, 2)
     means = (mock_mean, mean_6h1, mean_6h2)
     errors = [gene_details_map[gene]["mock"]["std"], gene_details_map[gene]["cond1"]["std"],
               gene_details_map[gene]["cond2"]["std"]]
@@ -55,12 +55,7 @@ def plot_gene(gene_details_map, gene, user_id, report_uuid):
     labels = ('mock', 'cond1', 'cond2')
     fig, ax = plt.subplots()
     rect = ax.bar(x_pos, means, 0.5, color='lightskyblue')
-    plotline1, caplines1, barlinecols1 = ax.errorbar(x_pos, means, yerr=errors, lolims=True, ls='None', color='black')
-
-    caplines1[0].set_marker('_')
-    caplines1[0].set_markersize(20)
-
-    ymax = max(means) + max(errors) * 1.5
+    # plotline1, caplines1, barlinecols1 = ax.er　
     plt.ylim(ymax=ymax)
     plt.xlabel(gene, fontsize="x-large")
     plt.ylabel('Relative Expression Ratio', fontsize="x-large")
