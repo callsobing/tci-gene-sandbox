51b15f305eb38d4f47506a93453c28913f1f6f48
callsobing@gmail.com <lemontree>
2018-12-16 12:36:15 +0800

    bar chart test

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -55,7 +55,12 @@ def plot_gene(gene_details_map, gene, user_id, report_uuid):
     labels = ('mock', 'cond1', 'cond2')
     fig, ax = plt.subplots()
     rect = ax.bar(x_pos, means, 0.5, color='lightskyblue')
-    # plotline1, caplines1, barlinecols1 = ax.er　
+    # plotline1, caplines1, barlinecols1 = ax.errorbar(x_pos, means, yerr=errors, lolims=True, ls='None', color='black')
+    #
+    # caplines1[0].set_marker('_')
+    # caplines1[0].set_markersize(20)
+
+    ymax = max(means) + max(errors) * 1.5
     plt.ylim(ymax=ymax)
     plt.xlabel(gene, fontsize="x-large")
     plt.ylabel('Relative Expression Ratio', fontsize="x-large")
