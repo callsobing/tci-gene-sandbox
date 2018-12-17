8232f204c5feff7532e4277b473cecc913d901d9
callsobing <callsobing@gmail.com>
2018-12-17 18:55:41 +0800

    debug

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -67,8 +67,9 @@ def expression_direction_score(direction, expression):
 
 def platform_score(gene_details_map, platforms):
     score_sum = 0
-    output_fh = open("reports/%s/%s/platform_score" % (user_id, report_uuid), "w")
+    output_fh = open("reports/%s/%s/platform_score" % (user_id, report_uuid), "w+")
     for platform in platforms:
+        output_fh.write(platforms)
         output_fh.write(platform)
         gene_count = 0.0
         for gene in platforms[platform]['up']:
