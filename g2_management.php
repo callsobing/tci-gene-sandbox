02c6bd685560eeebf85f12bcc4615925ae39099a
callsobing <callsobing@gmail.com>
2018-12-26 17:28:56 +0800

    update BCL-2 directions

--- g2_management.php
+++ g2_management.php
@@ -68,6 +68,14 @@ include 'check_login.php';
     <!-- Main CSS-->
     <link href="css/theme.css" rel="stylesheet" media="all">
 
+    <!-- pager plugin -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.1/css/jquery.tablesorter.pager.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.1/js/extras/jquery.tablesorter.pager.min.js"></script>
+    <style>
+        .tablesorter-pager .btn-group-sm .btn {
+            font-size: 1.2em; /* make pager arrows more visible */
+        }
+    </style>
 </head>
 
 <body class="animsition">
@@ -312,11 +320,11 @@ include 'check_login.php';
                                     <option value="膀胱癌">膀胱癌</option>
                                     <option value="淋巴癌">淋巴癌</option>
                                 </select>
+                                </div>
                                 <div class="row form-group">
                                     <button type="submit" name="submit" id="submit" class="btn btn-primary btn-sm">Submit</button>
                                     <button type="reset" class="btn btn-danger btn-sm">Reset</button>
                                 </div>
-                            </div>
                             </form>
                         </div>
                     </div>
@@ -401,9 +409,61 @@ include 'check_login.php';
 <script src="js/main.js"></script>
 <!-- 檢查表單正確性  -->
 <script>
-    $(document).ready(function() {
-        $('#data_table').DataTable();
-    } );
+    $(function() {
+
+        $("table").tablesorter({
+            theme : "bootstrap",
+
+            widthFixed: true,
+
+            // widget code contained in the jquery.tablesorter.widgets.js file
+            // use the zebra stripe widget if you plan on hiding any rows (filter widget)
+            // the uitheme widget is NOT REQUIRED!
+            widgets : [ "filter", "columns", "zebra" ],
+
+            widgetOptions : {
+                // using the default zebra striping class name, so it actually isn't included in the theme variable above
+                // this is ONLY needed for bootstrap theming if you are using the filter widget, because rows are hidden
+                zebra : ["even", "odd"],
+
+                // class names added to columns when sorted
+                columns: [ "primary", "secondary", "tertiary" ],
+
+                // reset filters button
+                filter_reset : ".reset",
+
+                // extra css class name (string or array) added to the filter element (input or select)
+                filter_cssFilter: [
+                    'form-control',
+                    'form-control',
+                    'form-control custom-select', // select needs custom class names :(
+                    'form-control',
+                    'form-control',
+                    'form-control',
+                    'form-control'
+                ]
+
+            }
+        })
+            .tablesorterPager({
+
+                // target the pager markup - see the HTML block below
+                container: $(".ts-pager"),
+
+                // target the pager page select dropdown - choose a page
+                cssGoto  : ".pagenum",
+
+                // remove rows from the table to speed up the sort of large tables.
+                // setting this to false, only hides the non-visible rows; needed if you plan to add/remove rows with the pager enabled.
+                removeRows: false,
+
+                // output string - default is '{page}/{totalPages}';
+                // possible variables: {page}, {totalPages}, {filteredPages}, {startRow}, {endRow}, {filteredRows} and {totalRows}
+                output: '{startRow} - {endRow} / {filteredRows} ({totalRows})'
+
+            });
+
+    });
 </script>
 </body>
 
