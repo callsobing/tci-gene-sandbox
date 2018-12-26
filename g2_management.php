c01a4682a91d180f8d9749baba8c56ad078f8db2
callsobing <callsobing@gmail.com>
2018-12-26 21:13:02 +0800

    update button

--- g2_management.php
+++ g2_management.php
@@ -318,7 +318,7 @@ include 'check_login.php';
 
                 <!-- 有收到資料才show下面欄位 -->
                 <!-- DATA TABLE-->
-                <p align="left"><button onclick="exportTableToExcel('customer_Data')">輸出為xls檔案</button></p>
+                <p align="right"><button onclick="exportTableToExcel('customer_Data')" class="button"><span>輸出為xls檔案</span></button></p>
                 <div class="card">
                     <table class="sortable" cellpadding="10" id="customer_Data">
                         <thead>
@@ -452,6 +452,48 @@ include 'check_login.php';
         }
     }
 </script>
+
+<style>
+    .button {
+        display: inline-block;
+        border-radius: 4px;
+        background-color: #f4511e;
+        border: none;
+        color: #FFFFFF;
+        text-align: center;
+        font-size: 28px;
+        padding: 20px;
+        width: 200px;
+        transition: all 0.5s;
+        cursor: pointer;
+        margin: 5px;
+    }
+
+    .button span {
+        cursor: pointer;
+        display: inline-block;
+        position: relative;
+        transition: 0.5s;
+    }
+
+    .button span:after {
+        content: '\00bb';
+        position: absolute;
+        opacity: 0;
+        top: 0;
+        right: -20px;
+        transition: 0.5s;
+    }
+
+    .button:hover span {
+        padding-right: 25px;
+    }
+
+    .button:hover span:after {
+        opacity: 1;
+        right: 0;
+    }
+</style>
 
 </body>
 
