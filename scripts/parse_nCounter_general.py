b07a530bf4b68a581af6b237b37c77800847d2c6
callsobing <callsobing@gmail.com>
2019-01-02 17:30:09 +0800

    update color

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -146,9 +146,9 @@ def plot_gene(gene_details_map, gene, user_id, report_uuid):
     fig, ax = plt.subplots()
     # plt.figure(figsize=(6, 7))
 
-    colors = ["b", "g", "r", "c", "m"]
-    plt.bar(x_pos, means, 1.0, color='lightskyblue', align='center', linewidth=0)
-    plotline1, caplines1, barlinecols1 = ax.errorbar(x_pos, means, yerr=errors, lolims=True, ls='None', color="black", barsabove=True)
+    colors = "bgrcmykw"
+    plt.bar(x_pos, means, 1.0, color=colors, align='center', linewidth=0)
+    plotline1, caplines1, barlinecols1 = ax.errorbar(x_pos, means, yerr=errors, lolims=True, ls='None', color='black', barsabove=True)
 
     caplines1[0].set_marker('.')
     caplines1[2].set_marker('_')
