5f2e63df71f0edce394afb52d4834ad1ed014a64
callsobing <callsobing@gmail.com>
2019-01-02 19:43:35 +0800

    using labels

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -147,9 +147,9 @@ def plot_gene(gene_details_map, gene, user_id, report_uuid):
     # plt.figure(figsize=(6, 7))
 
     colors = ["#3D3A4B", "#705D56", "#B19994", "#D3C0CD", "#E3DFFF"]
-    plt.bar(x_pos, means, 1.0, color=colors, align='center', linewidth=0, label=labels)
+    plt.bar(x_pos, means, 1.0, color=colors, align='center', linewidth=0)
     plotline1, caplines1, barlinecols1 = ax.errorbar(x_pos, means, yerr=errors, lolims=True, ls='None', color='black', barsabove=True)
-    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0.)
+    lgd = plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0., labels=labels)
 
     caplines1[0].set_marker('.')
     caplines1[2].set_marker('_')
