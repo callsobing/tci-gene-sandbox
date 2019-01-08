0bb821f5cefbd01fb0ec68c37e2a1bf882b49b5b
callsobing <callsobing@gmail.com>
2019-01-08 18:11:32 +0800

    update

--- scripts/gen_merged_figures.py
+++ scripts/gen_merged_figures.py
@@ -11,6 +11,8 @@ import sys
 plt.rcParams['font.sans-serif'] = ['SimHei']
 import codecs
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
 file_name = sys.argv[1]
 samples_fh = sys.argv[2]
