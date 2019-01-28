77bf0d401f39be13139e9ae2602d43ed3535c32f
callsobing <callsobing@gmail.com>
2019-01-28 17:39:25 +0800

    update structure

--- nCounter_result.php
+++ nCounter_result.php
@@ -473,7 +473,7 @@ include 'check_login.php';
                                         <br>
                                         <center><button type="submit" class="btn btn-primary btn-sm">產生報告模板</button>
                                             <button type="reset" class="btn btn-danger btn-sm">重新選擇</button>
-                                            <input type="button" onClick="parent.location='reports/<?php echo($user) ?>/<?php echo($uuid) ?>/output.xls'" value="下載xls資料" formtarget="_blank">
+                                            <button type="button" onClick="parent.location='reports/<?php echo($user) ?>/<?php echo($uuid) ?>/output.xls'" value="下載xls資料" formtarget="_blank">
                                         </center>
                                         <br>
                                     </div>
