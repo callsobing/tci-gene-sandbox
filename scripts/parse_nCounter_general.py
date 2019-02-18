edf6eebc8a6abc57a9d9ff94d080b079b7c04e7b
callsobing <callsobing@gmail.com>
2019-02-18 16:58:34 +0800

    output不太對靜的時候輸出0

--- scripts/parse_nCounter_general.py
+++ scripts/parse_nCounter_general.py
@@ -100,7 +100,10 @@ def platform_score(gene_details_map):
         if score_sum < 0:
             output_fh.write("%s\t0\n" % platform)
         else:
-            output_fh.write("%s\t%.2f\n" % (platform, (score_sum * 100 / (gene_count * 15))))
+            try:
+                output_fh.write("%s\t%.2f\n" % (platform, (score_sum * 100 / (gene_count * 15))))
+            except:
+                output_fh.write("%s\t0\n" % platform)
     output_fh.close()
 
 
